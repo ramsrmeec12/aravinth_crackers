2df001f331477c6ae53513bd6dc376d3543bddee
ramsrmeec12 <ramskv1018@gmail.com>
2025-08-20 23:58:36 +0530

    dd

--- src/components/InvoiceGenerator.jsx
+++ src/components/InvoiceGenerator.jsx
@@ -120,7 +120,7 @@ export default function InvoiceGenerator({ order, shopInfo }) {
       onClick={generatePDF}
       className="mt-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
     >
-      Download Invoice
+      Invoice
     </button>
   );
 }
